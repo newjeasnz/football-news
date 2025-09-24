50b1e9644220cc69d2fe873411f4210e9b5187f5
newjeasnz <jesiccatandra@gmail.com>
2025-09-24 16:03:40 +0700

    Ubah CSRF_TRUSTED_ORIGINS

--- football_news/settings.py
+++ football_news/settings.py
@@ -35,7 +35,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "jessica-tandra-footballnews.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://jessica-tandra-footballnews.pbp.cs.ui.ac.id/"
+    "https://jessica-tandra-footballnews.pbp.cs.ui.ac.id"
 ]
 
 # Application definition
